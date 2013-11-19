a6f4b67e9718b5a327e59ea796c46e0c0b0c2e33
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-11-19 20:27:27 +0400

    Add install targets (+ios) and hunter integration

---
+++ cmake/iOS.cmake
@@ -0,0 +1,8 @@
+# Mini toolchain file for install
+if(NOT XCODE_VERSION)
+  message(FATAL_ERROR "Xcode only!")
+endif()
+set(CMAKE_OSX_SYSROOT iphoneos)
+set(CMAKE_CXX_COMPILER_WORKS TRUE)
+set(CMAKE_C_COMPILER_WORKS TRUE)
+set(HUNTER_CMAKE_GENERATOR Xcode)
