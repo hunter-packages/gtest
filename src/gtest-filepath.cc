afc42c17489621787bf9414a206a9e66e8a8f9ce
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-06-22 15:41:09 +0400

    Suppress memory sanitizer warning

--- src/gtest-filepath.cc
+++ src/gtest-filepath.cc
@@ -342,11 +342,18 @@ FilePath FilePath::RemoveTrailingPathSeparator() const {
       : *this;
 }
 
+#if defined(__has_feature) && __has_feature(memory_sanitizer)
+# define ATTRIBUTE_NO_SANITIZER __attribute__((no_sanitize_memory))
+#else
+# define ATTRIBUTE_NO_SANITIZER
+#endif
+
 // Removes any redundant separators that might be in the pathname.
 // For example, "bar///foo" becomes "bar/foo". Does not eliminate other
 // redundancies that might be in a pathname involving "." or "..".
 // TODO(wan@google.com): handle Windows network shares (e.g. \\server\share).
-void FilePath::Normalize() {
+// no sanitizer: https://code.google.com/p/memory-sanitizer/issues/detail?id=57
+ATTRIBUTE_NO_SANITIZER void FilePath::Normalize() {
   if (pathname_.c_str() == NULL) {
     pathname_ = "";
     return;
